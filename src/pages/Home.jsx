c9d454cba4ae3738ad3e356b2c2b94c068137007
Artur <artur98games@mail.ru>
2023-06-20 18:16:13 +0700

    no message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import Card from "../components/Card/Card";
+import Card from '../components/Card/Card';
 
 function Home({
   cards,
@@ -14,10 +14,9 @@ function Home({
 }) {
   const renderCards = () => {
     const filterCards = cards.filter((item) =>
-      item.name.toLowerCase().includes(searchValue.toLowerCase())
-      
+      item.name.toLowerCase().includes(searchValue.toLowerCase()),
     );
-    console.log(filterCards)
+    console.log(filterCards);
     return (isLoading ? [...Array(8)] : filterCards).map((item, i) => (
       <Card
         key={i}
@@ -29,29 +28,30 @@ function Home({
       />
     ));
   };
-  
+
   return (
     <div className="content">
       <div className="content-block">
-        <h1>
-          {searchValue ? `Поиск по запросу: ${searchValue}` : "Все консоли"}
-        </h1>
-        <div className="search-block">
-          <img src="/img/search.svg" alt="search" />
-          {searchValue ? (
-            <img
-              onClick={onSearchValue}
-              className="basket-clear"
-              src="/img/btn-remove.svg"
-              alt="close"
-            />
-          ) : null}
-          <input
-            onChange={onChangeSearchInput}
-            value={searchValue}
-            placeholder="Поиск"
-          />
-        </div>
+        <h1>{searchValue ? `Поиск по запросу: ${searchValue}` : 'Все консоли'}</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSearchValue();
+          }}
+        >
+          <div className="search-block">
+            <img src="/img/search.svg" alt="search" />
+            {searchValue ? (
+              <img
+                onClick={onSearchValue}
+                className="basket-clear"
+                src="/img/btn-remove.svg"
+                alt="close"
+              />
+            ) : null}
+            <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск" />
+          </div>
+        </form>
       </div>
       <div className="gameconsole">{renderCards()}</div>
     </div>
